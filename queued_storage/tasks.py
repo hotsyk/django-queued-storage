f32851718beadf45888361ddf7d5f07dd941bb18
Volodymyr Hotsyk <gotsyk@gmail.com>
2010-11-08 18:07:38 +0200

    Added RSA encryption of the saved file

--- queued_storage/tasks.py
+++ queued_storage/tasks.py
@@ -1,14 +1,40 @@
 from django.core.cache import cache
 from django.core.files.storage import get_storage_class
+from django.conf import settings
 
 from celery.registry import tasks
 from celery.task import Task
 
+
+from ncrypt.cipher import EncryptCipher, DecryptCipher
+from ncrypt.rsa import RSAKey
+
+def encrypt_file(rsa_key, in_file, out_file):
+    while 1 :
+        data = in_file.read(8192)
+        if not data: break
+        out_data = rsa_key.encrypt(data)
+        out_file.write(out_data)
+
+            
 class SaveToRemoteTask(Task):
     def run(self, name, local, remote, cache_key):
         local_storage = get_storage_class(local)()
         remote_storage = get_storage_class(remote)()
-        remote_storage.save(name, local_storage.open(name))
+        #encrypt
+        if settings.ENCRYPT_UPLOADED_FILES:
+            key = RSAKey()
+            # Read in a public key
+            fd = open(settings.CRYPTO_KEYS_PUBLIC, "rb")
+            public_key = fd.read()
+            fd.close()
+            # import this public key
+            key.fromPEM_PublicKey(public_key)            
+            encrypt_file(key, local_storage.open(name),\
+                local_storage.open(name, 'w'))
+            remote_storage.save(name, local_storage.open(name))
+        else:
+            remote_storage.save(name, local_storage.open(name))
         cache.set(cache_key, True)
         return True
         
